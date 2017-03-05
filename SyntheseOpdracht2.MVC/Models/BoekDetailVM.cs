31ec7272f7857cc5f469394a3e3ba46e34f6090c
Nadia Curinckx <nadia_curinckx@hotmail.com>
2017-03-05 10:57:36 +0100

    Trash and pencil

--- SyntheseOpdracht2.MVC/Models/BoekDetailVM.cs
+++ SyntheseOpdracht2.MVC/Models/BoekDetailVM.cs
@@ -10,6 +10,8 @@ namespace SyntheseOpdracht2.MVC.Models
         public Int32 Id { get; set; }
         public String Titel { get; set; }
         public String Auteur { get; set; }
+
+        [DisplayName("Aantal pagina's")]
         public Int32 AantalPaginas { get; set; }
 
         [DisplayName("Genres")]
